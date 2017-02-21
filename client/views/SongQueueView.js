386a1ad9b62abf362e0640b41673a788c793859f
hchenfred <hchen.fred@gmail.com>
2017-02-20 17:52:34 -0800

    Implement SongQueueView to pass tests

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,22 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.collection.on('add ended dequeue', function() {
+      this.render();
+    }, this);
   },
 
   render: function() {
+    // clear out past queue view
+    this.$el.empty();
+    // loops through collection and passes each to renderSong
+    this.collection.forEach(this.renderSong, this);
     return this.$el;
-  }
+  },
+
+  renderSong: function(song) {
+    var songView = new SongQueueEntryView({model: song});
+    this.$el.append(songView.render());
+  } 
 
 });
